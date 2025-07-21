2ad9bee6f98bc0a1867e3e9ea6c71bff93665e80
mohammed imtiyaz <mohammedimtiyaz.jk@yahoo.com>
2025-07-21 17:07:46 +0530

    added preferance data to db

--- app/(tabs)/Home.jsx
+++ app/(tabs)/Home.jsx
@@ -1,6 +1,18 @@
+import { useRouter } from "expo-router";
+import { useContext, useEffect } from "react";
 import { Text, View } from "react-native";
-
+import { UserContext } from "../../context/UserContext";
 const Home = () => {
+  const router = useRouter();
+  const { user } = useContext(UserContext);
+
+  useEffect(() => {
+    // This effect runs when the component mounts or when 'user' changes
+    if (!user?.weight) {
+      router.push("/preferance");
+    }
+  }, [user]);
+
   return (
     <View>
       <Text>Home</Text>
--- shared/color.jsx
+++ shared/color.jsx
@@ -1,5 +1,10 @@
-export default {WHITE: '#FFFFFF', PRIMARY:"#8837ff",BLACK: '#000000', RED: '#FF0000', GREEN: '#00FF00', BLUE: '#0000FF', YELLOW: '#FFFF00', CYAN: '#00FFFF', MAGENTA: '#FF00FF', GRAY: '#808080', LIGHT_GRAY: '#D3D3D3', DARK_GRAY: '#A9A9A9'};
-export const TRANSPARENT = 'rgba(0, 0, 0, 0)';
-export const LIGHT_BLUE = '#ADD8E6';
-export const DARK_BLUE = '#00008B';
-export const ORANGE = '#FFA500';                 
+export default {
+  WHITE: "#fff",
+  PRIMARY: "#7e22ce",
+  GRAY: "gray",
+  BLUE: "#4287f5",
+  PINK: "#F542b3",
+  GREEN: "#39b981",
+  SECONDERY: "#fbf5ff",
+  RED: "red",
+};
